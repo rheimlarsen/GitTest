0a204ae8dbb1b73352e0d119f15557e0d9f8e37e
RheimLarsen <rheimlarsen@gmail.com>
2017-08-14 12:23:31 -0400

    Delelted some stuff and branched to implement "Cool Stuff"

--- test.swift
+++ test.swift
@@ -1,6 +1,7 @@
-//This is a test
-
 
 //This is a second test comment. Trying out a new function
 
 //Putting in a third comment, but this one I'm trying to delete the first comment, cause it sucked.
+
+
+//I think I branched out on this one.
